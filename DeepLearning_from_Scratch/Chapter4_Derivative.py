75edf96cc25db0aa8d581febc5a193edf642fa1a
taekwang94 <58813265+taekwang94@users.noreply.github.com>
2020-01-31 18:57:57 +0900

    Create Chapter4_Derivative.py
    practice

---
+++ DeepLearning_from_Scratch/Chapter4_Derivative.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def numerical_diff(f,x):
+    h = 1e-4
+    return (f(x+h)-f(x-h))/(2*h)
+
+def numerical_gradient(f,x):
+    h = 1e-4
+    grad = np.zeros_like(x) # x와 형상이 같은 zero 배열
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx]=tmp_val+h
+        fxh1 = f(x)
+
+        x[idx] =tmp_val -h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1-fxh2)/(2*h)
+        x[idx] = tmp_val
+
+    return grad
+
+def gradient_descent(f, init_x, lr=0.01, step_num =100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f,x)
+        x -=lr*grad
+    return x
