ceccfed5e867bf9958b04daedfa043a0cffd0c8d
taekwang94 <58813265+taekwang94@users.noreply.github.com>
2020-01-31 17:10:04 +0900

    Update Chapter3_MNIST_3.6.py
    Compare time

--- DeepLearning_from_Scratch/Chapter3_MNIST_3.6.py
+++ DeepLearning_from_Scratch/Chapter3_MNIST_3.6.py
@@ -1,9 +1,13 @@
 import sys, os
 import numpy as np
 import pickle
+import time
 sys.path.append(os.pardir) # 부모 디렉터리 파일 가져올 수 있도록 설정
 from dataset.mnist import load_mnist
 from NN_chapter3 import sigmoid,softmax,relu
+
+
+
 def get_data():
     (x_train , y_train) , (x_test, y_test) = load_mnist(flatten=True,normalize=True,one_hot_label=False)
     return x_test, y_test
@@ -40,6 +44,7 @@ batch_size = 100
 
 def predict_test(batch=False):
     if batch==True:
+        start = time.time()
         accuracy_cnt = 0
         for i in range(0, len(x_test), batch_size):
             x_batch = x_test[i:i+batch_size]
@@ -48,8 +53,10 @@ def predict_test(batch=False):
             p = np.argmax(y_batch,axis=1)
             accuracy_cnt += np.sum(p==y_test[i:i+batch_size])
 
-        print("Accuracy: " + str(float(accuracy_cnt / len(x_test))))
+        print("Accuracy : " + str(float(accuracy_cnt / len(x_test))))
+        print("Time (batch O): ", time.time() - start)
     else:
+        start = time.time()
         accuracy_cnt = 0
         for i in range(len(x_test)):
             y = predict(network, x_test[i], Active='sigmoid')
@@ -58,8 +65,8 @@ def predict_test(batch=False):
             if p == y_test[i]:
                 accuracy_cnt += 1
 
-        print("Accuracy: " + str(float(accuracy_cnt / len(x_test))))
-
+        print("Accuracy : " + str(float(accuracy_cnt / len(x_test))))
+        print("Time (batch X): ", time.time() - start)
 
 predict_test()
 predict_test(True)
